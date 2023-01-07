aee3bc2dfe7ac0ee26db014a915c4ae677188cc1
Adarshgb <105109356+Adarshgb@users.noreply.github.com>
2023-01-07 14:25:33 +0530

    Delete demoapp directory

--- demoapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from demoapp import models
-
-admin.site.register(models.log)
--- demoapp/userviews.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-from demoapp.models import userdata
-
-
-def profileview(request):
-    u=request.user
-    data=userdata.objects.filter(user=u)
-    return render(request,'usertemp/profileview.html',{'data':data})
